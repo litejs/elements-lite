49b9267ec541ceef006eb20f6887c91bc3aa83f8
Lauri Rooden <lauri@rooden.ee>
2013-05-16 15:16:01 +0300

    Add responsive.js

---
+++ responsive.js
@@ -0,0 +1,30 @@
+
+!function(w) {
+  var lastSize, lastOrient
+	, b = document.body
+	, emit = function(){ w.M && M.emit("resize") }.rate(200, true)
+
+  function setClasses() {
+    var width = b.offsetWidth
+		, next = width < 601 ? "s-mob" : ( width < 1025 ? "s-tab" : "s-full")
+
+    if ( next != lastSize ) {
+      b.addClass( next ).rmClass( lastSize )
+			lastSize = next
+    }
+
+    next = width > b.offsetHeight ? "s-land" : "s-port"
+
+    if ( next != lastOrient) {
+      b.addClass( next ).rmClass( lastOrient )
+			lastOrient = next
+    }
+		emit()
+  }
+	setClasses()
+
+	Event.add(w, "resize", setClasses)
+	Event.add(b, "orientationchange", setClasses)
+	Event.add(w, "load", setClasses)
+}(this)
+
